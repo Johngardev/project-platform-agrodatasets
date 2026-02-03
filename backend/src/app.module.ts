382aa90c0b51b10986a9576020261d4dfc728bc5
Johngardev <johngarlo@icloud.com>
2026-02-02 21:37:10 -0500

    feat(backend): exposición de recursos estáticos y endpoint de detalle

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/require-await */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
@@ -7,6 +9,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { DatasetsModule } from './datasets/datasets.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -19,6 +23,10 @@ import { DatasetsModule } from './datasets/datasets.module';
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URI'),
       }),
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'uploads'), // Sube 2 niveles desde dist/src para llegar a uploads
+      serveRoot: '/uploads', // Las imágenes se verán en localhost:3000/uploads/foto.jpg
     }),
     UsersModule,
     AuthModule,
