57e834da806810075a1f8f401e865d78a51d7c8a
Johngardev <johngarlo@icloud.com>
2026-03-25 17:02:42 -0500

    feat(frontend): se actualizan los dataset-card mostrando la informacion relevante de cada dataset incluyendo los dddatos reales

--- frontend/src/app/features/dashboard/pages/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/features/dashboard/pages/admin-dashboard/admin-dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { DatasetCardComponent } from '../../../../shared/components/dataset-card/dataset-card.component';
 import { DatasetService } from '../../../../core/services/dataset.service';
 import { RouterLink } from "@angular/router";
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [DatasetCardComponent, RouterLink],
+  imports: [DatasetCardComponent, RouterLink, DatePipe],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
--- frontend/src/app/shared/components/dataset-card/dataset-card.component.ts
+++ frontend/src/app/shared/components/dataset-card/dataset-card.component.ts
@@ -17,4 +17,26 @@ export class DatasetCardComponent {
   @Input() imageUrl!: string; // URL de la imagen (puede ser local o remota)
   @Input() year!: string;
   @Input() imageCount!: string;
+  @Input() status!: string;
+
+  getStatusClasses(status: string | undefined): string {
+    if (!status) return 'bg-gray-500/10 text-gray-400 border-gray-500/20'; // Default style for unknown status
+
+    const s = status.toUpperCase();
+
+    switch (s) {
+      case 'APPROVED':
+        return 'bg-emerald-500/10 text-emerald-600 border-emerald-500/20';
+
+      case 'REJECTED':
+        return 'bg-red-500/10 text-red-600 border-red-500/20';
+
+      case 'PENDING':
+        return 'bg-amber-500/10 text-amber-500 border-amber-500/20';
+      
+      default:
+        // Por defecto o cualquier otro estado, usamos gris
+        return 'bg-gray-500/10 text-gray-400 border-gray-500/20';
+    }
+  }
 }
