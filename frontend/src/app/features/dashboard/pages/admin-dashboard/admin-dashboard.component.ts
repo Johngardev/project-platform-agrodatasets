500b2ac4d68cc28333c81804ab87d91973c89148
Johngardev <johngarlo@icloud.com>
2026-03-25 22:36:15 -0500

    feat(frontend): se actulizan los stats del admin-dashboard

--- frontend/src/app/features/dashboard/pages/admin-dashboard/admin-dashboard.component.ts
+++ frontend/src/app/features/dashboard/pages/admin-dashboard/admin-dashboard.component.ts
@@ -2,12 +2,12 @@ import { Component, inject } from '@angular/core';
 import { DatasetCardComponent } from '../../../../shared/components/dataset-card/dataset-card.component';
 import { DatasetService } from '../../../../core/services/dataset.service';
 import { RouterLink } from "@angular/router";
-import { DatePipe } from '@angular/common';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [DatasetCardComponent, RouterLink, DatePipe],
+  imports: [DatasetCardComponent, RouterLink, DatePipe, DecimalPipe],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
@@ -15,9 +15,30 @@ export class AdminDashboardComponent {
   datasetService = inject(DatasetService);
   datasets: any[] = [];
 
+  stats = {
+    pending: 0,
+    approved: 0,
+    totalImages: 0
+  }
+
   ngOnInit() {
-    this.datasetService.getDatasets().subscribe(data => {
-      this.datasets = data;
+    this.datasetService.getDatasets().subscribe({
+      next: (data) => {
+        this.datasets = data;
+        this.calculateStats(); // Llamamos al cálculo después de recibir la data
+      },
+      error: (err) => console.error('Error cargando datasets', err)
     });
   }  
+
+  calculateStats() {
+    // 1. Contamos los pendientes
+    this.stats.pending = this.datasets.filter(ds => ds.status === 'PENDING').length;
+    
+    // 2. Contamos los aprobados
+    this.stats.approved = this.datasets.filter(ds => ds.status === 'APPROVED').length;
+    
+    // 3. Sumamos todas las imágenes usando reduce
+    this.stats.totalImages = this.datasets.reduce((sum, ds) => sum + (ds.image_count || 0), 0);
+  }
 }
